eaf4f71008c811758e61484230904b8cfeb2c2c7
JahongirHamidov <hamidovjahongir444@gmail.com>
2021-03-12 23:53:37 +0500

    admin products control added

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -5,7 +5,10 @@ import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_DETAILS_FAIL,
     PRODUCT_DETAILS_REQUEST,
-    PRODUCT_DETAILS_SUCCESS 
+    PRODUCT_DETAILS_SUCCESS ,
+    PRODUCT_DELETE_FAIL,
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_DELETE_SUCCESS,
 } from "../constants/productConstants"
 
 export const listProducts = () => async (dispatch) => {
@@ -46,4 +49,35 @@ export const listProductDetails = (id) => async (dispatch) => {
         })
     }
 }
+
+
+
+export const deleteProduct = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: PRODUCT_DELETE_REQUEST
+        })
+
+        const {userLogin:{userInfo}} = getState()
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        await axios.delete(`/api/products/${id}`, config)
+        dispatch({
+            type: PRODUCT_DELETE_SUCCESS,
+        })
+
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DELETE_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+
 
